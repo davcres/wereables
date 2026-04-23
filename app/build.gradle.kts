78b072bcb8ab295dd0148aac0d79db4181dc63b5
David <david@A224.local>
2026-04-23 11:05:30 +0200

    update app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.davidcrespo.ble"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
